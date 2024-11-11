20ae0b7000282af714f5241721578a9fd257c592
sahil <shsasane22@gmail.com>
2024-11-11 13:01:57 +0530

    commit

--- Data Structures/practice/bstprac.py
+++ Data Structures/practice/bstprac.py
@@ -7,12 +7,12 @@ class BST:
     def add_child(self, data):
         if data == self.data:
             return
-        elif data < self.data:
+        if data < self.data:
             if self.left:
                 self.left.add_child(data)
             else:
                 self.left = BST(data)
-        elif data > self.data:
+        if data > self.data:
             if self.right:
                 self.right.add_child(data)
             else:
@@ -21,12 +21,12 @@ class BST:
     def search(self, data):
         if self.data == data:
             return True
-        elif data < self.data:
+        if data < self.data:
             if self.left:
                 return self.left.search(data)
             else:
                 return False
-        elif data > self.data:
+        if data > self.data:
             if self.right:
                 return self.right.search(data)
             else:
@@ -41,29 +41,21 @@ class BST:
             elements += self.right.inorder()
         return elements
 
+    def preorder(self):
+        elements = []
+        elements.append(self.data)
+        if self.left:
+            elements += self.left.inorder()
+        if self.right:
+            elements += self.right.inorder()
+        return elements
+
     def find_max(self):
-        if self.right is None:
+        if not self.right:
             return self.data
         return self.right.find_max()
 
-    def delete(self, data):
-        if data < self.data:
-            if self.left:
-                self.left = self.left.delete(data)
-        elif data > self.data:
-            if self.right:
-                self.right = self.right.delete(data)
-        else:
-            if self.right is None and self.left is None:
-                return None
-            if self.right is None:
-                return self.left
-            if self.left is None:
-                return self.right
-            max_val = self.left.find_max()
-            self.data = max_val
-            self.left = self.left.delete(max_val)
-        return self
+    # def delete(self, data):
 
 
 def buildBST(elements):
@@ -79,7 +71,6 @@ if __name__ == "__main__":
     elements = [4, 2, 7, 1, 3, 6, 9]
     tree = buildBST(elements)
     print(tree.inorder())
-    # print(tree.preorder())
+    print(tree.preorder())
     print(tree.search(1))
-    tree.delete(3)
     print(tree.inorder())
